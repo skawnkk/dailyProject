68b6577a61ed7d6d37419fce3bbb9fe642a6f6ed
skawnkk <skawn11114@gmail.com>
2023-06-28 23:25:40 +0900

    [feat] mutate 이후 setQueryData

--- apps/my-daily/src/api/daily.ts
+++ apps/my-daily/src/api/daily.ts
@@ -1,16 +1,17 @@
-import {useMutation, useQuery} from '@tanstack/react-query'
-
-export const useGetDaily = (id: string) => {
+import {UseQueryResult, useMutation, useQuery, useQueryClient} from '@tanstack/react-query'
+import {Daily} from '../types/daily'
+export const useGetDaily = (id: string): UseQueryResult<Daily> => {
   return useQuery(['daily'], async () => {
-    const data = await fetch(`http://localhost:8080/schedule/daily/${id}`)
+    const data = await fetch(`${process.env.NEXT_PUBLIC_SERVER}/schedule/daily/${id}`)
     return data.json()
   })
 }
 
 export const useUpdateTodo = (dailyId: string) => {
+  const queryClient = useQueryClient()
   const mutation = useMutation({
     mutationFn: async ({todoId, value}: {todoId: string; value: string}) => {
-      const res = await fetch(`http://localhost:8080/schedule/daily/${dailyId}/todo`, {
+      return fetch(`${process.env.NEXT_PUBLIC_SERVER}/schedule/daily/${dailyId}/todo`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -20,8 +21,15 @@ export const useUpdateTodo = (dailyId: string) => {
           value,
         }),
       })
-
-      console.log(res)
+    },
+    onSuccess: async data => {
+      const {id, done} = await data.json()
+      const previousData = queryClient.getQueryData(['daily']) as Daily
+      const updatedTodos = previousData.todos.map(todo => {
+        return todo.todo_id === id ? {...todo, done} : todo
+      })
+      queryClient.setQueryData(['daily'], (prev: Daily) => ({...prev, todos: updatedTodos}))
+      return {previousData}
     },
   })
 
