9e80472e5cf514c62fa721fea422c4946c7c1bef
김남주 <namju.kim@tictoccroc.com>
2023-07-01 15:07:39 +0900

    [refactor] api 분리 및 query key 관리

---
+++ apps/my-daily/src/api/api.ts
@@ -0,0 +1,15 @@
+const baseUrl = process.env.NEXT_PUBLIC_SERVER
+export const api = {
+  get: async (url: string) => {
+    return await fetch(baseUrl + url)
+  },
+  post: async (url: string, body: unknown) => {
+    return fetch(baseUrl + url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+  },
+}
--- apps/my-daily/src/api/monthly.ts
+++ apps/my-daily/src/api/monthly.ts
@@ -1,8 +1,9 @@
 import {useQuery} from '@tanstack/react-query'
+import {api} from './api'
 
 export const useGetMonthlyList = () => {
   return useQuery(['monthly'], async () => {
-    const data = await fetch('http://localhost:8080/schedule/monthly')
+    const data = await api.get('/schedule/monthly')
     return data.json()
   })
 }
