20f417a51aa4f26208c85b899e7f1362a52a6511
skawnkk <skawn11114@gmail.com>
2023-07-06 00:22:29 +0900

    [feat][error] enter 치고 데이터 펫칭 로직 실행, but 에러있음

--- apps/my-daily/src/app/daily/[id]/TodoInput.tsx
+++ apps/my-daily/src/app/daily/[id]/TodoInput.tsx
@@ -6,16 +6,29 @@ import {useInput} from '../../../hooks/useInput'
 function TodoInput() {
   const {id} = useParams()
   const {mutate} = useCreateTodo(id)
-  const {value, onChange} = useInput()
+  const {value, onChange, reset} = useInput()
   const addTodo = () => {
     mutate(value)
+    reset()
   }
-  const handleKeyDown = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      addTodo()
+    }
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     onChange(e.target.value)
   }
   return (
     <div>
-      <input value={value} placeholder="오늘 해야 할 일을 입력하세요." onChange={handleKeyDown} />
+      <input
+        value={value}
+        contentEditable
+        placeholder="오늘 해야 할 일을 입력하세요."
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
       <button onClick={addTodo}>+</button>
     </div>
   )
