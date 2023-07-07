c5993da5fa9bc42af132a7d4cdc978d5790c78f4
skawnkk <skawn11114@gmail.com>
2023-07-07 19:34:06 +0900

    [refactor] TodoInput > form tag로 변경

--- apps/my-daily/src/app/daily/[id]/TodoInput.tsx
+++ apps/my-daily/src/app/daily/[id]/TodoInput.tsx
@@ -1,35 +1,25 @@
 import {useParams} from 'next/navigation'
-import React from 'react'
 import {useCreateTodo} from '../../../api/daily'
-import {useInput} from '../../../hooks/useInput'
 
 function TodoInput() {
   const {id} = useParams()
   const {mutate} = useCreateTodo(id)
-  const {value, onChange, reset} = useInput()
-  const addTodo = () => {
-    mutate(value)
-    reset()
-  }
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      addTodo()
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const value = e.target[0].value
+    if (!value) {
+      return
     }
+    mutate(value)
+    e.target[0].value = ''
   }
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    onChange(e.target.value)
-  }
   return (
     <div>
-      <input
-        value={value}
-        contentEditable
-        placeholder="오늘 해야 할 일을 입력하세요."
-        onChange={handleChange}
-        onKeyDown={handleKeyDown}
-      />
-      <button onClick={addTodo}>+</button>
+      <form onSubmit={addTodo}>
+        <input placeholder="오늘 해야 할 일을 입력하세요." />
+        <button type="submit">+</button>
+      </form>
     </div>
   )
 }
